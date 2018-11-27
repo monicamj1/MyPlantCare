7133ea34a2f6056371f86eedcf951bd7e6f6a24d
Mónica Moreno <mmoreno856@gmail.com>
2018-11-27 08:32:00 +0100

    AddPlantActivity creado.

---
+++ app/src/main/java/com/example/monicamj1/myplantcare/AddPlantActivity.java
@@ -0,0 +1,13 @@
+package com.example.monicamj1.myplantcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPlantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_plant);
+    }
+}
