7c59f45e06c1b2f7ae3071b18fe4adfb0e61ef3b
Misericòrdia Gironés <mesegirones97@gmail.com>
2018-11-27 09:59:12 +0100

    Parte del MyPlantActivity layout

--- app/src/main/java/com/example/monicamj1/myplantcare/MyPlantActivity.java
+++ app/src/main/java/com/example/monicamj1/myplantcare/MyPlantActivity.java
@@ -2,6 +2,10 @@ package com.example.monicamj1.myplantcare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class MyPlantActivity extends AppCompatActivity {
 
@@ -10,4 +14,12 @@ public class MyPlantActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_plant);
     }
+
+    ImageView profileImage = findViewById(R.id.profileImage_view);
+
+   /* Glide.with(this)
+            //.load("file:///android_asset/profile.png")
+            .apply(RequestOptions.circleCropTransform())
+            .into(profileImage);*/
+
 }
