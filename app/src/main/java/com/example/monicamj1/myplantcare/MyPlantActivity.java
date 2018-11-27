38ce3ab43da127b89b5840d9862afb5c5c7f0bba
Misericòrdia Gironés <mesegirones97@gmail.com>
2018-11-27 08:27:49 +0100

    Mi primer comiiitt!! MyPlantActivity creado.

---
+++ app/src/main/java/com/example/monicamj1/myplantcare/MyPlantActivity.java
@@ -0,0 +1,13 @@
+package com.example.monicamj1.myplantcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyPlantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_plant);
+    }
+}
