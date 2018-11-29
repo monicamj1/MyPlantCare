aadd89df52792c8d92def7eaa3214b338424cdc6
Misericòrdia Gironés <mesegirones97@gmail.com>
2018-11-29 14:03:38 +0100

    Tema de colors + imatges retallades

--- app/src/main/java/com/example/monicamj1/myplantcare/MyPlantActivity.java
+++ app/src/main/java/com/example/monicamj1/myplantcare/MyPlantActivity.java
@@ -5,21 +5,28 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.request.RequestOptions;
 
 public class MyPlantActivity extends AppCompatActivity {
+
+
+    private ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_plant);
+
+       profileImage = findViewById(R.id.profileImage_view);
+
+        Glide.with(this)
+                .load("file:///android_asset/flor.png")
+                .apply(RequestOptions.circleCropTransform())
+                .into(profileImage);
     }
 
-    ImageView profileImage = findViewById(R.id.profileImage_view);
 
-   /* Glide.with(this)
-            //.load("file:///android_asset/profile.png")
-            .apply(RequestOptions.circleCropTransform())
-            .into(profileImage);*/
+
 
 }
