3c4b90ae3e8783bd14175eb90655a09a80ddaf62
misericordiagd <mesegirones97@gmail.com>
2018-12-01 12:41:20 +0100

    MyPlantActivity rellena condatos de la clase Plant. Algunos problemas con las fechas y la imagen, per eso las lineas estan comentadas.

--- app/src/main/java/com/example/monicamj1/myplantcare/MyPlantActivity.java
+++ app/src/main/java/com/example/monicamj1/myplantcare/MyPlantActivity.java
@@ -3,14 +3,17 @@ package com.example.monicamj1.myplantcare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.request.RequestOptions;
 
+import java.util.Date;
+
 public class MyPlantActivity extends AppCompatActivity {
 
-
+    Plant myPlant;
     private ImageView profileImage;
 
     @Override
@@ -18,12 +21,31 @@ public class MyPlantActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_plant);
 
+        TextView namePlant = findViewById(R.id.name_view);
+        TextView specieName = findViewById(R.id.cientific_view);
+        TextView birthday = findViewById(R.id.birthday_view);
+        TextView watering = findViewById(R.id.watering_view);
+        TextView waterDays = findViewById(R.id.waterDays_view);
+
+        myPlant = new Plant("Lola la flora", "Desconocido", "",
+                new Date(2018,11,18), 5,
+                new Date(2018,11,18), null,
+                "http://www.mijardin.es/wp-content/uploads/2017/01/cultivar-la-planta-del-dinero.jpg");
        profileImage = findViewById(R.id.profileImage_view);
 
         Glide.with(this)
-                .load("file:///android_asset/flor.png")
+                .load(myPlant.getProfile()) //problemas con el get de la imagen
                 .apply(RequestOptions.circleCropTransform())
                 .into(profileImage);
+
+        //TODO:calcular dias que faltan para regar
+        //TODO: fechas bien!!
+
+        namePlant.setText(myPlant.getName());
+        specieName.setText(myPlant.getScientific_name());
+       // birthday.setText(myPlant.getBirthday().toString());
+        //watering.setText(myPlant.getReminder());
+       // waterDays.setText(myPlant.getLast_watering_day().toString());
     }
 
 
