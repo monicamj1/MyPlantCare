aa4fa71fa5b2296367fb6821e892a33d8a8499ea
Mónica Moreno <mmoreno856@gmail.com>
2018-12-14 18:44:39 +0100

    Arreglados detalles

--- app/src/main/java/com/example/monicamj1/myplantcare/MyPlantActivity.java
+++ app/src/main/java/com/example/monicamj1/myplantcare/MyPlantActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Camera;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
